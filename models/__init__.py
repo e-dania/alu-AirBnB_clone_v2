d80a9780356b14b459c33b1c0ee0324a152c5adf
e-dania <156061632+e-dania@users.noreply.github.com>
2024-09-27 23:23:09 +0200

    corrections

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
@@ -15,5 +15,5 @@ from os import getenv
 if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
-storage = FileStorage()
+    storage = FileStorage()
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
 
+
 class Amenity(BaseModel, Base):
- """This is the class for Amenity
+    """This is the class for Amenity
     Attributes:
         name: input name
     """
