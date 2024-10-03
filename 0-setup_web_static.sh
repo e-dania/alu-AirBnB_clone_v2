a9c992ff79a2f43433ceea9620fcfd874cb0c112
e-dania <156061632+e-dania@users.noreply.github.com>
2024-10-04 00:32:31 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-sudo echo "<h1>Emmanuel Dania</h1>" > /data/web_static/releases/test/index.html
+sudo echo "<h1>Emmanuel Dania</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
