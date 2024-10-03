d89f11e1394e9589c53f0fa45e8174980570c06f
Emmanuel Dania <e.dania@alustudent.com>
2024-10-03 21:31:46 +0000

    step1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+sudo echo "<h1>Emmanuel Dania</h1>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
